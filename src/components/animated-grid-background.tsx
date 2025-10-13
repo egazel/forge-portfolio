5b650561af0b2a83ad10b43df56af5db11c57d0b
Elie GAZEL <inscriptiondofus87@gmail.com>
2025-10-13 13:30:34 +0000

    Make the animated background grid a bit more visible

--- src/components/animated-grid-background.tsx
+++ src/components/animated-grid-background.tsx
@@ -14,7 +14,7 @@ export function AnimatedGridBackground({
         className
       )}
     >
-      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,hsl(var(--primary)/0.1)_1px,transparent_1px),linear-gradient(to_bottom,hsl(var(--primary)/0.1)_1px,transparent_1px)] bg-[size:30px_30px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
+      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,hsl(var(--primary)/0.15)_1px,transparent_1px),linear-gradient(to_bottom,hsl(var(--primary)/0.15)_1px,transparent_1px)] bg-[size:30px_30px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
     </div>
   );
 }
