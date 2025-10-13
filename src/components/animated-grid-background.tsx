a1d23deb31019fdfd015d319a6bd243c923c9fcc
Elie GAZEL <inscriptiondofus87@gmail.com>
2025-10-13 07:39:16 +0000

    On the background we went from 30 to 10, can you put to 20

--- src/components/animated-grid-background.tsx
+++ src/components/animated-grid-background.tsx
@@ -14,7 +14,7 @@ export function AnimatedGridBackground({
         className
       )}
     >
-      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808010_1px,transparent_1px),linear-gradient(to_bottom,#80808010_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
+      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,#80808020_1px,transparent_1px),linear-gradient(to_bottom,#80808020_1px,transparent_1px)] bg-[size:24px_24px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
     </div>
   );
 }
