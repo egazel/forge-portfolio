8b7892e13da73453ba0e510ab3264df30cfd9273
inscriptiondofus87 <inscriptiondofus87@gmail.com>
2025-12-10 17:25:53 +0000

    Background update

--- src/components/animated-grid-background.tsx
+++ src/components/animated-grid-background.tsx
@@ -14,7 +14,7 @@ export function AnimatedGridBackground({
         className
       )}
     >
-      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,hsl(var(--primary)/0.25)_2px,transparent_2px),linear-gradient(to_bottom,hsl(var(--primary)/0.25)_2px,transparent_2px)] bg-[size:45px_45px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
+      <div className="absolute inset-0 h-full w-full bg-[linear-gradient(to_right,hsl(var(--primary)/0.25)_2px,transparent_2px),linear-gradient(to_bottom,hsl(var(--primary)/0.25)_2px,transparent_2px)] bg-[size:55px_55px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)] animate-grid"></div>
     </div>
   );
 }
